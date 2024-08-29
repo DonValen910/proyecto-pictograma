c3363312d08df65c8880290872b693c6ff98576c
valentinstito <valentinstito@gmail.com>
2024-08-29 00:15:41 +0000

    Update todo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,174 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(/*private nativeAudio: NativeAudio*/) { }
+/*
+  ionViewWillEnter() {
+    this.nativeAudio.preloadSimple('adios', 'assets/audio/adios.mp3');
+    this.nativeAudio.preloadSimple('algomemolesta', 'assets/audio/algomemolesta.mp3');
+    this.nativeAudio.preloadSimple('asustado', 'assets/audio/asustado.mp3');
+    this.nativeAudio.preloadSimple('ayuda', 'assets/audio/ayuda.mp3');
+    this.nativeAudio.preloadSimple('baño', 'assets/audio/baño.mp3');
+    this.nativeAudio.preloadSimple('bien', 'assets/audio/bien.mp3');
+    this.nativeAudio.preloadSimple('corregir', 'assets/audio/corregir.mp3');
+    this.nativeAudio.preloadSimple('enfermo', 'assets/audio/enfermo.mp3');
+    this.nativeAudio.preloadSimple('gracias', 'assets/audio/gracias.mp3');
+    this.nativeAudio.preloadSimple('hola', 'assets/audio/hola.mp3');
+    this.nativeAudio.preloadSimple('levantarlamano', 'assets/audio/levantarlamano.mp3');
+    this.nativeAudio.preloadSimple('megustaesto', 'assets/audio/megustaesto.mp3');
+    this.nativeAudio.preloadSimple('mesientomal', 'assets/audio/mesientomal.mp3');
+    this.nativeAudio.preloadSimple('muchoruido', 'assets/audio/muchoruido.mp3');
+    this.nativeAudio.preloadSimple('no', 'assets/audio/no.mp3');
+    this.nativeAudio.preloadSimple('noentiendo', 'assets/audio/noentiendo.mp3');
+    this.nativeAudio.preloadSimple('nometoques', 'assets/audio/nometoques.mp3');
+    this.nativeAudio.preloadSimple('nopuedo', 'assets/audio/nopuedo.mp3');
+    this.nativeAudio.preloadSimple('nose', 'assets/audio/nose.mp3');
+    this.nativeAudio.preloadSimple('notengolonecesario', 'assets/audio/notengolonecesario.mp3');
+    this.nativeAudio.preloadSimple('perdon', 'assets/audio/perdon.mp3');
+    this.nativeAudio.preloadSimple('preocupado', 'assets/audio/preocupado.mp3');
+    this.nativeAudio.preloadSimple('sed', 'assets/audio/sed.mp3');
+    this.nativeAudio.preloadSimple('si', 'assets/audio/si.mp3');
+    this.nativeAudio.preloadSimple('sipuedo', 'assets/audio/sipuedo.mp3');
+    this.nativeAudio.preloadSimple('tengoqueirme', 'assets/audio/tengoqueirme.mp3');
+    this.nativeAudio.preloadSimple('terminado', 'assets/audio/terminado.mp3');
+  }
 
+  play1() {
+    this.nativeAudio.play('adios');
+  }
+
+  play2() {
+    this.nativeAudio.play('algomemolesta');
+  }
+
+  play3() {
+    this.nativeAudio.play('asustado');
+  }
+
+  play4() {
+    this.nativeAudio.play('ayuda');
+  }
+
+  play5() {
+    this.nativeAudio.play('baño');
+  }
+
+  play6() {
+    this.nativeAudio.play('bien');
+  }
+
+  play7() {
+    this.nativeAudio.play('corregir');
+  }
+
+  play8() {
+    this.nativeAudio.play('enfermo');
+  }
+
+  play9() {
+    this.nativeAudio.play('gracias');
+  }
+
+  play10() {
+    this.nativeAudio.play('hola');
+  }
+
+  play11() {
+    this.nativeAudio.play('levantarlamano');
+  }
+
+  play12() {
+    this.nativeAudio.play('megustaesto');
+  }
+
+  play13() {
+    this.nativeAudio.play('mesientomal');
+  }
+
+  play14() {
+    this.nativeAudio.play('muchoruido');
+  }
+
+  play15() {
+    this.nativeAudio.play('no');
+  }
+
+  play16() {
+    this.nativeAudio.play('noentiendo');
+  }
+
+  play17() {
+    this.nativeAudio.play('nometoques');
+  }
+
+  play18() {
+    this.nativeAudio.play('nopuedo');
+  }
+
+  play19() {
+    this.nativeAudio.play('nose');
+  }
+
+  play20() {
+    this.nativeAudio.play('notengolonecesario');
+  }
+
+  play21() {
+    this.nativeAudio.play('perdon');
+  }
+
+  play22() {
+    this.nativeAudio.play('preocupado');
+  }
+
+  play23() {
+    this.nativeAudio.play('sed');
+  }
+
+  play24() {
+    this.nativeAudio.play('si');
+  }
+
+  play25() {
+    this.nativeAudio.play('sipuedo');
+  }
+
+  play26() {
+    this.nativeAudio.play('tengoqueirme');
+  }
+
+  play27() {
+    this.nativeAudio.play('terminado');
+  }
+
+  ionViewDidLeave() {
+    this.nativeAudio.unload('adios');
+    this.nativeAudio.unload('algomemolesta');
+    this.nativeAudio.unload('asustado');
+    this.nativeAudio.unload('ayuda');
+    this.nativeAudio.unload('baño');
+    this.nativeAudio.unload('bien');
+    this.nativeAudio.unload('corregir');
+    this.nativeAudio.unload('enfermo');
+    this.nativeAudio.unload('gracias');
+    this.nativeAudio.unload('hola');
+    this.nativeAudio.unload('levantarlamano');
+    this.nativeAudio.unload('megustaesto');
+    this.nativeAudio.unload('mesientomal');
+    this.nativeAudio.unload('muchoruido');
+    this.nativeAudio.unload('no');
+    this.nativeAudio.unload('noentiendo');
+    this.nativeAudio.unload('nometoques');
+    this.nativeAudio.unload('nopuedo');
+    this.nativeAudio.unload('nose');
+    this.nativeAudio.unload('notengolonecesario');
+    this.nativeAudio.unload('perdon');
+    this.nativeAudio.unload('preocupado');
+    this.nativeAudio.unload('sed');
+    this.nativeAudio.unload('si');
+    this.nativeAudio.unload('sipuedo');
+    this.nativeAudio.unload('tengoqueirme');
+    this.nativeAudio.unload('terminado');
+  }
+  */
 }
